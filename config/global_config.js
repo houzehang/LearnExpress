debe3df3a5c7fe81d7fb190374c64ef174d592bb
侯泽航 <2210036910@qq.com>
2018-06-24 20:19:38 +0800

    背景fixed

--- config/global_config.js
+++ config/global_config.js
@@ -1,9 +1,9 @@
 "use strict";
 exports.goodskinds = [
 	 { id : 1, name : '零食/饮料'}
-	,{ id : 2, name : '图书'}
+	,{ id : 2, name : '生活用品'}
 	,{ id : 3, name : '交通工具'}
-	,{ id : 4, name : '生活用品'}
+	,{ id : 4, name : '图书'}
 	,{ id : 5, name : '电子产品'}
 	,{ id : 6, name : '代跑腿'}
 	,{ id : 7, name : '网上服务'}
